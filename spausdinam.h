8f9096f683249224cdc195559f0e838e2c788680
BrotatoTornado <mykoloasmeninis@gmail.com>
2026-04-23 23:36:47 +0300

    Update spausdinam.h

--- spausdinam.h
+++ spausdinam.h
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef SPAUSDINAM_H
+#define SPAUSDINAM_H
 
 #include "konteineris.h"
 
@@ -8,3 +10,5 @@ void spausdinam(const StudContainer& studis, const std::string& pav);
 void rikiuotiStudentus(StudContainer& studis, const std::string& target);
 void surikiuotiStudentus(StudContainer& studis, int pasirinkimas);
 void surikiuotiPagalGalutiniVid(StudContainer& studis);
+
+#endif
