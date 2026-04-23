85e621d87e29eae7d00e43d505fad16ed914c61c
BrotatoTornado <mykoloasmeninis@gmail.com>
2026-04-23 23:35:17 +0300

    Update laikai.h

--- laikai.h
+++ laikai.h
@@ -1,14 +1,18 @@
 #pragma once
+#ifndef LAIKAI_H
+#define LAIKAI_H
 
 struct TimerResults
 {
-    double generavimas = 0;
-    double skaitymas = 0;
-    double rusiavimas = 0;
-    double skirstymas = 0;
-    double isvedimas = 0;
+    double generavimas = 0.0;
+    double skaitymas = 0.0;
+    double rusiavimas = 0.0;
+    double skirstymas = 0.0;
+    double isvedimas = 0.0;
 };
 
 extern TimerResults timers;
 
 void nunulintiLaikus();
+
+#endif
