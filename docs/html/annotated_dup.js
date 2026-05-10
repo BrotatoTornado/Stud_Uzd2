7e1ebb7131212008463cb0583cae597cabe913ad
BrotatoTornado <mykoloasmeninis@gmail.com>
2026-05-10 10:43:57 -0400

    Add files via upload

---
+++ docs/html/annotated_dup.js
@@ -0,0 +1,8 @@
+var annotated_dup =
+[
+    [ "Skirstymorezultatas", "structSkirstymorezultatas.html", "structSkirstymorezultatas" ],
+    [ "Studentas", "classStudentas.html", "classStudentas" ],
+    [ "TimerResults", "structTimerResults.html", "structTimerResults" ],
+    [ "TyrimoIrasas", "structTyrimoIrasas.html", "structTyrimoIrasas" ],
+    [ "Zmogus", "classZmogus.html", "classZmogus" ]
+];
