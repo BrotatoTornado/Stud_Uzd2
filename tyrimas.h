5544909221584b6682aeebeec4f239f6ca50c294
BrotatoTornado <mykoloasmeninis@gmail.com>
2026-05-10 16:50:45 +0300

    Update tyrimas.h

--- tyrimas.h
+++ tyrimas.h
@@ -1,3 +1,26 @@
+/**
+ * @file tyrimas.h
+ * @brief Konteinerių ir skirstymo strategijų greitaveikos tyrimo funkcijos.
+ *
+ * Atlieka laiko matavimus skaitant failus, rikiuojant ir skirstant studentus
+ * bei išveda rezultatus į konsolę ir Markdown lentelę.
+ *
+ * @author Studentas
+ * @version 2.0
+ */
+
 #pragma once
+
+ /**
+  * @brief Paleidžia interaktyvų konteinerių greitaveikos tyrimą.
+  *
+  * Vartotojas gali pasirinkti:
+  * - skirstymo strategiją;
+  * - kartojimų skaičių (rezultatai vidurkinami);
+  * - testuojamą failą arba standartinį failų rinkinį (1k–10M eilučių).
+  *
+  * Rezultatai išvedami į konsolę ir išsaugomi kaip Markdown lentelė
+  * faile `benchmark_<konteineris>_S<strategija>.md`.
+  */
 
 void vykdytiTyrima();
