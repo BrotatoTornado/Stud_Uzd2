a44ce7d94a0055f65b1886c1de50b4bc1ac308c5
BrotatoTornado <mykoloasmeninis@gmail.com>
2026-05-17 23:58:03 +0300

    Update laikai.h

--- laikai.h
+++ laikai.h
@@ -1,4 +1,21 @@
+/**
+ * @file laikai.h
+ * @brief Laiko matavimų struktūra ir pagalbinės funkcijos.
+ *
+ * Apibrėžia globalų `timers` objektą, kuriame kaupiami kiekvienos
+ * programos fazės trukmės matavimai.
+ *
+ * @author Studentas
+ * @version 3.0
+ */
+
+
 #pragma once
+
+ /**
+  * @struct TimerResults
+  * @brief Saugo kiekvienos programos fazės laiko matavimus (sekundėmis).
+  */
 
 struct TimerResults
 {
@@ -9,6 +26,13 @@ struct TimerResults
     double isvedimas = 0.0;
 };
 
+/** @brief Globalus laiko matavimų objektas. */
 extern TimerResults timers;
+
+/**
+ * @brief Nulinina visus laiko matavimus.
+ *
+ * Iškviečiama kiekvienos naujos iteracijos pradžioje.
+ */
 
 void nunulintiLaikus();
