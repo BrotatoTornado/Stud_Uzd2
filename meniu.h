ec41d0c747921c447b96b34fb611ecbd8132aa51
BrotatoTornado <mykoloasmeninis@gmail.com>
2026-05-17 23:59:07 +0300

    Update meniu.h

--- meniu.h
+++ meniu.h
@@ -1,5 +1,32 @@
+/**
+ * @file meniu.h
+ * @brief Pagrindinio programos meniu funkcijos deklaracija.
+ *
+ * @author Studentas
+ * @version 3.0
+ */
+
 #pragma once
 
 #include "konteineris.h"
+
+/**
+ * @brief Paleidžia pagrindinį interaktyvų programos meniu.
+ *
+ * Leidžia vartotojui pasirinkti vieną iš šių veiksmų:
+ * - įvesti studentą rankiniu būdu;
+ * - nuskaityti studentus iš failo;
+ * - pridėti atsitiktinius studentus;
+ * - generuoti duomenų failą;
+ * - atlikti konteinerių greitaveikos tyrimą;
+ * - paleisti klasės unit testus;
+ * - tikrinti Zmogus abstraktumą;
+ * - baigti darbą.
+ *
+ * @param studis Studentų konteineris, į kurį pridedami / keičiami duomenys.
+ * @return `true` jei reikia tęsti programą (grįžti į pagrindinį ciklą),
+ *         `false` jei vartotojas pasirinko baigti darbą.
+ * @throws std::runtime_error Klaidos įvesties atveju.
+ */
 
 bool vykdytiMeniu(StudContainer& studis);
